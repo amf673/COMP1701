d5275e47bef5851cfb9bc5dd3915a433fafeeb12
amf673 <afedoruk@mtroyal.ca>
2023-11-29 09:39:59 -0700

    file to list

--- filetolist.py
+++ filetolist.py
@@ -1,9 +1,12 @@
 
 
-f = open("data.txt", "r")
+f = open("data1.txt", "r")
 list_of_lines = []
 for line in f: 
     list_of_lines.append(line)
 
-print(list_of_lines)
+i = 0
+for line in list_of_lines: 
+    i = i + 1
+    print(f"{i}. {len(line)} {line[0:15]}...")
 f.close()
